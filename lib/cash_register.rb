c35999030b32edf73ea28ff919647f688bb3f86b
tania <taniaosano15@gmail.com>
2022-10-25 17:49:09 +0300

    Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    attr_reader :discount, :total , :price, :quantity, :title, :items   #read only
+    attr_accessor :accumulated_price #reads and writes
+    def initialize (discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+         @accumulated_price = 0
+    end
+    def total=(total)
+    @total = total
+    end
+    def add_item(title, price, quantity=1)
+        self.accumulated_price = price * quantity
+        self.total += self.accumulated_price
+        quantity.times {self.items << title}
 
+    end 
+    def apply_discount
+        if self.discount > 0
+        self.total = self.total - (self.total * self.discount/100)
+        "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    def void_last_transaction
+    self.total -= self.accumulated_price
+    end
+end
+purchasing = CashRegister.new
